b35bac0b6a73cdf14a975ee5a629f1c7d65f03ba
Tanish Bin Masum <137204730+TanishBinMasum@users.noreply.github.com>
2025-10-09 19:29:24 +0600

    Update Profile.jsx
    nothing much really, just changed the wording to sound more fancy

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -57,8 +57,8 @@ export default function Profile({ isDarkMode, toggleDarkMode }) {
 
           <div className="max-w-2xl mx-auto">
             <p className="text-lg md:text-xl leading-relaxed text-muted-foreground font-mono">
-              I'm a web developer passionate about turning ideas into simple,
-              responsive, and functional websites that help businesses grow.
+              I'm a web developer passionate about turning ideas into simple yet elegant,
+              responsive yet clean, and beautiful websites that help businesses grow.
             </p>
           </div>
         </div>
